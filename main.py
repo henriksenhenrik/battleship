1167b29ed8b7fbaef1290fb0d075db80286aa9b3
henriksenhenrik <henhen17@vgs.nfk.no>
2022-12-08 13:39:38 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,47 @@
+import pygame
 
+# initialize pygame and create a game window
+pygame.init()
+game_window = pygame.display.set_mode((800, 600))
+pygame.display.set_caption("My Game")
+
+# set the background color
+bg_color = (0, 0, 0)
+
+# load and display game assets, such as images and sounds
+# ...
+
+# create a game loop
+while True:
+    # handle events
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.QUIT()
+            exit()
+            
+        # handle user input events, such as keyboard and mouse
+    if event.type == pygame.KEYDOWN:
+        if event.key == pygame.K_ESCAPE:
+            # pause the game
+            paused = True
+            while paused:
+                for event in pygame.event.get():
+                    if event.type == pygame.KEYDOWN:
+                        if event.key == pygame.K_ESCAPE:
+                            pygame.QUIT()
+
+    # update game logic and mechanics
+    # ...
+
+    # draw game assets to the screen
+    game_window.fill(bg_color)
+    # ...
+
+    # update the game display
+    pygame.display.flip()
+
+    # check for game over and win conditions
+    
+
+# clean up and exit gracefully
+pygame.QUIT()
